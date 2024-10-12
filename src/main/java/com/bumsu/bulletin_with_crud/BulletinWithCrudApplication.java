83831feed6741b403866d29e198cb42f742da0d8
bumsu Choe <hotsem100@gmail.com>
2024-10-12 10:37:02 +0900

    #6 - 스프링 부트 프로젝트 시작
    들어간 기술:
    
    * 스프링 웹
    * 엑츄에이터
    * 데브툴즈
    * 롬복

---
+++ src/main/java/com/bumsu/bulletin_with_crud/BulletinWithCrudApplication.java
@@ -0,0 +1,13 @@
+package com.bumsu.bulletin_with_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulletinWithCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BulletinWithCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bumsu/bulletin_with_crud/BulletinWithCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bumsu.bulletin_with_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulletinWithCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

ab9ff77a8f29b5f4bd068a9088af82b327f21ea7
bumsu Choe <hotsem100@gmail.com>
2024-10-14 17:17:50 +0900

    #13 - AuditingFields 추출
    생성자, 생성일시, 수정자, 수정일시는 반복적으로 엔티티 클래스에 들어가는 요소이고, 도메인과 직접 연관이 없는 요소임으로 추출함
    
    `@MappedSupdrclass` 이용하여 상속 방식으로 추출함

--- src/main/java/com/bumsu/bulletin_with_crud/repository/ArticleCommentRepository.java
+++ src/main/java/com/bumsu/bulletin_with_crud/repository/ArticleCommentRepository.java
@@ -2,7 +2,9 @@ package com.bumsu.bulletin_with_crud.repository;
 
 import com.bumsu.bulletin_with_crud.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 
 }
--- src/main/java/com/bumsu/bulletin_with_crud/repository/ArticleRepository.java
+++ src/main/java/com/bumsu/bulletin_with_crud/repository/ArticleRepository.java
@@ -2,7 +2,10 @@ package com.bumsu.bulletin_with_crud.repository;
 
 import com.bumsu.bulletin_with_crud.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 
 }
